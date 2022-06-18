8df56a02d65bf2cf6f8a5a7cb3faa79d0aa912c2
swiser <swiser@linkedin.com>
2022-06-18 02:17:07 -0400

    Added fetch NFTs

--- pages/index.jsx
+++ pages/index.jsx
@@ -8,8 +8,8 @@ const Home = () => {
   const fetchNFTs = async() => {
     let nfts; 
     console.log("fetching nfts");
-    const api_key = "pku8X9d8U3r5hg5TTtkXZY-uwSfd7vd1"
-    const baseURL = `https://eth-mainnet.alchemyapi.io/v2/pku8X9d8U3r5hg5TTtkXZY-uwSfd7vd1/getNFTs/`;
+    const api_key = "pku8X9d8U3r5hg5TTtkXZY-uwSfd7vd1" // TODO read from configs
+    const baseURL = `https://eth-mainnet.alchemyapi.io/v2/${api_key}/getNFTs/`;
     var requestOptions = {
         method: 'GET'
       };
@@ -25,6 +25,7 @@ const Home = () => {
       nfts= await fetch(fetchURL, requestOptions).then(data => data.json())
     }
   
+    console.log(nfts)
     if (nfts) {
       console.log("nfts:", nfts)
       setNFTs(nfts.ownedNfts)
@@ -34,11 +35,11 @@ const Home = () => {
   return (
     <div className="flex flex-col items-center justify-center py-8 gap-y-3">
       <div className="flex flex-col w-full justify-center items-center gap-y-2">
-        <input type={"text"} placeholder="Add your wallet address"></input>
-        <input type={"text"} placeholder="Add the collection address"></input>
-        <label className="text-gray-600 "><input type={"checkbox"} className="mr-2"></input>Fetch for collection</label>
+        <input onChange={(e)=>{setWalletAddress(e.target.value)}} value={wallet} type={"text"} placeholder="Add your wallet address"></input>
+        <input onChange={(e)=>{setCollectionAddress(e.target.value)}} value={collection} type={"text"} placeholder="Add the collection address"></input>
         <button className={"disabled:bg-slate-500 text-white bg-blue-400 px-4 py-2 mt-3 rounded-sm w-1/5"} onClick={
           () => {
+            fetchNFTs()
           }
         }>Let's go! </button>
       </div>
